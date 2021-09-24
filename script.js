004bf1662c79197bfd5f11cfbd81ec6a89a7128b
Eric Maihato Candido <91287410+ericcandido@users.noreply.github.com>
2021-09-24 14:19:29 -0300

    arquivos de certificard adicionados

---
+++ script.js
@@ -0,0 +1,6 @@
+// Terminar todo o design que está no figma (disponível abaixo)
+// Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+
+function mudaModo() {
+  document.body.classList.toggle("dark");
+}
